f92a2ed9c0a6efdb809fcd318723bb3f08fd108d
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2023-12-12 18:09:16 -0300

    bugbug

--- Cadastro/cadastros/settings.py
+++ Cadastro/cadastros/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-4$)7&ygj!%q*h_%-&ijr-t2&iba9-(%w1hmz@2*z&r!9wtyo_8')
+SECRET_KEY = 'django-insecure-4$)7&ygj!%q*h_%-&ijr-t2&iba9-(%w1hmz@2*z&r!9wtyo_8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
