bf2bbb2a58f2c16b9e05d16d8db72ed91c9da422
cafuso102 <170146243@aluno.unb.br>
2023-12-14 10:42:25 -0300

    adicionado agenda

--- lib/pages/agenda.dart
+++ lib/pages/agenda.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:modernlogintute/screens/calendar_sceen.dart';
 
-class Agenda extends StatelessWidget {
+class Agenda extends StatefulWidget {
   const Agenda({Key? key}) : super(key: key);
 
   @override
+  _AgendaState createState() => _AgendaState();
+}
+
+class _AgendaState extends State<Agenda> {
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting('pt_BR', null).then((_) {
+      setState(() {
+        _isInitialized = true;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (!_isInitialized) {
+      return Container(); // Ou algum indicador de carregamento
+    }
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Agenda'),
@@ -14,13 +37,17 @@ class Agenda extends StatelessWidget {
             Navigator.of(context).pop(); // Volta para a página anterior
           },
         ),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: const Center(
-        child: Text(
-          'Conteúdo da Página',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      body: CalendarScreen(),
+      backgroundColor: Colors.white,
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Agenda(),
+  ));
 }
