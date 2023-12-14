5c663ed335787e85c7014d9f59a599995e72fe53
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2023-12-14 01:46:17 -0300

    rezando para funcionar

--- Cadastro/cadastros/settings.py
+++ Cadastro/cadastros/settings.py
@@ -40,6 +40,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
