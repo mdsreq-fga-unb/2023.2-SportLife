07f130b3135254bc3e3ae9fc51b2441ce7ce89cd
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2023-12-14 09:18:00 -0300

    ola

--- Cadastro/cadastros/settings.py
+++ Cadastro/cadastros/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "12345")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG ='RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME: 
